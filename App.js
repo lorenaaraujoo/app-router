af9b6d7bf23c32ece01b86ee15f8d8ea39addb0c
lorenaaraujoo <lorenaaraujo88183@gmail.com>
2023-06-06 11:59:06 -0300

    Primeira versão - router!

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
+
+  const [name, setName] = useState("Lorena")
+  const [input, setInput] = useState()
+
+  useEffect(() => {
+    async function searchName(){
+      const storageName = await AsyncStorage.getItem('name');
+
+      if (storageName !== null){
+        setName(storageName)
+      }
+    }
+
+    searchName()
+  }, [])
+
+  useEffect(() => {
+    async function recordName(){
+      await AsyncStorage.setItem('name', name)
+    }
+
+    recordName();
+  }, [name])
+
+  function switchName() {
+     setName(input)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style = {styles.input} onChangeText = {(text)=> setInput(text)}></TextInput>
+      <Button style = {styles.button} title = "Switch Name" onPress={switchName}></Button>
+      <Text style = {{fontSize: 20, marginTop: 10}}>{name}</Text>
     </View>
   );
 }
@@ -14,7 +44,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  button: {
+    width: '100%',
+    marginBottom: 15
+  },
+
+  input: {
+    borderWidth: 2,
+    marginBottom: 5,
+    marginTop: 10,
+    borderColor: 'black',
+    width: '100%'
+  }
 });
